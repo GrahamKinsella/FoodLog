3db2677924a9461129858e1f9455f05e042abd67
GrahamKinsella <graham_kinsella3@hotmail.com>
2020-10-16 18:35:15 +0100

    removing tests until after reviewing moq

--- FoodLogTests/FoodControllerTests.cs
+++ FoodLogTests/FoodControllerTests.cs
@@ -1,7 +1,6 @@
 using FoodLog.Contexts;
 using FoodLog.Models;
 using FoodLog.Repositories;
-using FoodLogTests.Mocks;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
@@ -13,122 +12,6 @@ namespace FoodLogTests
 {
     public class FoodControllerTests
     {
-        private Mock<IFoodContext> _context;
-        private FoodRepository _repo;
-        [Fact]
-        public void TestGetFood()
-        {
-            //TODO: Move Mock db setup to a base class
-            FoodRequest foodRequest = new FoodRequest
-            {
-                Name = "Banana"
-            };
-
-            var foods = new List<Food>
-            {
-                new Food
-                {
-                    Id = 1,
-                    Name = "Banana",
-                    Calories = 100,
-                    Carbohydrates = 10,
-                    Fats = 1,
-                    Protein = 3
-
-                }
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Food>>();
-            mockSet.As<IQueryable<Food>>().Setup(m => m.Provider).Returns(foods.Provider);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.Expression).Returns(foods.Expression);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.ElementType).Returns(foods.ElementType);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.GetEnumerator()).Returns(foods.GetEnumerator());
-
-            _context = new Mock<IFoodContext>();
-            _context.Setup(c => c.Foods).Returns(mockSet.Object);
-            _repo = new FoodRepository(_context.Object);
-
-            Assert.Single(_repo.GetFoods(foodRequest));
-
-        }
-
-        [Fact]
-        public void TestAddFood()
-        {
-            //TODO: Move Mock db setup to a base class
-            Food food = new Food
-            {
-                Name = "Apple",
-                Calories = 110,
-                Carbohydrates = 15,
-                Fats = 11,
-                Protein = 10
-            };
-
-            var foods = new List<Food>
-            {
-                new Food
-                {
-                    Id = 1,
-                    Name = "Banana",
-                    Calories = 100,
-                    Carbohydrates = 10,
-                    Fats = 1,
-                    Protein = 3
-
-                }
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Food>>();
-            mockSet.As<IQueryable<Food>>().Setup(m => m.Provider).Returns(foods.Provider);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.Expression).Returns(foods.Expression);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.ElementType).Returns(foods.ElementType);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.GetEnumerator()).Returns(foods.GetEnumerator());
-
-            _context = new Mock<IFoodContext>();
-            _context.Setup(c => c.Foods).Returns(mockSet.Object);
-            _repo = new FoodRepository(_context.Object);
-
-            _repo.CreateFood(food);
-           //Write Assertion
-        }
-
-        [Fact]
-        public void TestDeleteFood()
-        {
-            //TODO: Move Mock db setup to a base class
-            FoodRequest food = new FoodRequest
-            {
-                Id = 1
-            };
-
-            var foods = new List<Food>
-            {
-                new Food
-                {
-                    Id = 1,
-                    Name = "Banana",
-                    Calories = 100,
-                    Carbohydrates = 10,
-                    Fats = 1,
-                    Protein = 3
-
-                }
-            }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Food>>();
-            mockSet.As<IQueryable<Food>>().Setup(m => m.Provider).Returns(foods.Provider);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.Expression).Returns(foods.Expression);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.ElementType).Returns(foods.ElementType);
-            mockSet.As<IQueryable<Food>>().Setup(m => m.GetEnumerator()).Returns(foods.GetEnumerator());
-
-            _context = new Mock<IFoodContext>();
-            _context.Setup(c => c.Foods).Returns(mockSet.Object);
-            _repo = new FoodRepository(_context.Object);
-
-            _repo.Delete(food);
-            //Write assertion
-
-        }
+       
     }
 }

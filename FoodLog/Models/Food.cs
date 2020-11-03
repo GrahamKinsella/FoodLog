9bab99b7efb034131d452a84ef80ec86cfe6964f
graham kinsella <graham_kinsella3@hotmail.com>
2020-11-03 22:22:00 +0000

    Create document interfaces to start repository pattern

--- FoodLog/Models/Food.cs
+++ FoodLog/Models/Food.cs
@@ -1,24 +1,15 @@
 ﻿using Google.Cloud.Firestore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FoodLog.Models
 {
     [FirestoreData]
-    public class Food
+    public abstract class Food : IFood
     {
         public int Id { get; set; }
-        [FirestoreProperty]
         public string Name { get; set; }
-        [FirestoreProperty]
         public int Calories { get; set; }
-        [FirestoreProperty]
         public int Carbohydrates { get; set; }
-        [FirestoreProperty]
         public int Protein { get; set; }
-        [FirestoreProperty]
         public int Fats { get; set; }
 
     }
